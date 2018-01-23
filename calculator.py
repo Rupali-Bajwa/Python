8db8b3393a5812f888adac3f51d596eea46a5c5d
rupali.php50@gmail.com <rupali.php50@gmail.com>
2018-01-24 00:35:30 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#Calculator to perform arithmetic operations
+#Addition
+x=2
+print (x+5)
+#subtraction
+a=4
+print (a-9)
+#multiplication
+q=7
+print (q*7)
+#division
+x=8
+print (x/3)
+#remove fractional part
+a=9
+print (a//2)
+
